506fed619943a5d9f27416805cc80b14ef8bfe22
chirag-singh1 <66097224+chirag-singh1@users.noreply.github.com>
2021-04-26 21:38:37 -0700

    Continuing to set up scaffolding

---
+++ server.js
@@ -0,0 +1,26 @@
+var mysql=require('mysql2')
+
+var con=mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    database: "recipes"
+});
+
+con.connect(function(err){
+    if(err) throw err;
+    console.log("Connected to MySQL Database");
+});
+
+const http = require('http');
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  con.query("SELECT * FROM INGREDIENTS",function(err,result){
+    if(err)throw err;
+        res.end(JSON.stringify(result));
+    });
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
