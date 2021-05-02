cf51f914c5e10576c6b28acbabd3e44fa64be948
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2021-05-02 02:32:24 -0700

    non dependent tables

--- reload_db.js
+++ reload_db.js
@@ -71,8 +71,8 @@ async function create_ingredients(con, json) {
   return new Promise((resolve, reject) => {
     console.log("Creating table ingredients...");
     const sql = `CREATE TABLE IF NOT EXISTS ingredients (
-      ingredient_id int NOT NULL AUTO_INCREMENT,
-      name varchar(255),
+      ingredient_id INT NOT NULL AUTO_INCREMENT,
+      name VARCHAR(255),
       PRIMARY KEY (ingredient_id),
       UNIQUE (name)
     ); TRUNCATE TABLE ingredients`;
@@ -88,6 +88,102 @@ async function create_ingredients(con, json) {
       }));
       console.log("Ingredients inserted");
       resolve();
+    });
+  });
+}
+
+async function insert_recipe(con, title, url, fat, salt, saturates, sugars) {
+  return new Promise((resolve, reject) => {
+    const sql = `INSERT INTO recipes (title, url, fat, salt, saturates, sugars)
+    VALUES ('${title}', '${url}', '${fat}', '${salt}', '${saturates}', '${sugars}')`;
+    con.query(sql, (err, res) => {
+      if (err) throw err;
+      resolve();
+    });
+  });
+}
+
+async function create_recipes(con, json) {
+  return new Promise((resolve, reject) => {
+    console.log("Creating table recipes...");
+    const sql = `CREATE TABLE IF NOT EXISTS recipes (
+      recipe_id INT NOT NULL AUTO_INCREMENT,
+      title TEXT,
+      url TEXT,
+      fat ENUM('green', 'orange', 'red'),
+      salt ENUM('green', 'orange', 'red'),
+      saturates ENUM('green', 'orange', 'red'),
+      sugars ENUM('green', 'orange', 'red'),
+      PRIMARY KEY (recipe_id)
+    ); TRUNCATE TABLE recipes`;
+    con.query(sql, (err, res) => {
+      if (err) throw err;
+      console.log("Table recipes created");
+
+      console.log("Inserting recipes...");
+      Promise.all(json.map((recipe) => {
+        await insert_recipe(con, recipe.title, recipe.url,
+          recipe.fsa_lights_per100g.fat,
+          recipe.fsa_lights_per100g.salt,
+          recipe.fsa_lights_per100g.saturates,
+          recipe.fsa_lights_per100g.sugars
+        );
+      }));
+      console.log("Recipes inserted");
+      resolve();
+    });
+  });
+}
+
+
+
+async function create_users(con) {
+  return new Promise((resolve, reject) => {
+    console.log("Creating table users...");
+    const sql = `CREATE TABLE IF NOT EXISTS users (
+      user_id INT NOT NULL AUTO_INCREMENT,
+      password VARCHAR(255) NOT NULL,
+      PRIMARY KEY (user_id)
+    ); TRUNCATE TABLE users`;
+    con.query(sql, (err, res) => {
+      if (err) throw err;
+      console.log("Table users created");
+      resolve();
+    });
+  });
+}
+
+async function create_user_tags(con) {
+  return new Promise((resolve, reject) => {
+    console.log("Creating table user_tags...");
+    const sql = `CREATE TABLE IF NOT EXISTS user_tags (
+      id int NOT NULL AUTO_INCREMENT,
+      user_id INT NOT NULL,
+      tag TEXT,
+      PRIMARY KEY (id),
+      FOREIGN KEY (user_id)
+    ); TRUNCATE TABLE user_tags`;
+    con.query(sql, (err, res) => {
+      if (err) throw err;
+      console.log("Table user_tags created");
+      resolve();
+    });
+  });
+}
+
+async function create_user_ingredients(con) {
+  return new Promise((resolve, reject) => {
+    console.log("Creating table user_ingredients...");
+    const sql = `CREATE TABLE IF NOT EXISTS user_ingredients (
+      id int NOT NULL AUTO_INCREMENT,
+      ingredient_id int NOT NULL,
+      amount int,
+      PRIMARY KEY (id),
+      FOREIGN KEY (ingredient_id)
+    ); TRUNCATE TABLE user_ingredients`;
+    con.query(sql, (err, res) => {
+      if (err) throw err;
+      console.log("Table user_ingredients created");
     });
   });
 }
@@ -111,7 +207,14 @@ async function reload_db() {
 
   // Reload database
   await create_db(con);
-  await create_ingredients(con, json);
+  create_users(con);
+  create_user_tags(con);
+  create_user_ingredients(con);
+  Promise.all([
+    await create_ingredients(con, json),
+    await create_recipes(con, json)
+  ]);
+  
 
   console.log("reload_db finished successfully");
 }
@@ -119,80 +222,3 @@ async function reload_db() {
 reload_db().catch((err) => {
   console.error(err.message);
 });
-/*
-          console.log("Creating table ingredients...");
-          sql =
-          `CREATE TABLE IF NOT EXISTS ingredients (
-            ingredient_id int NOT NULL AUTO_INCREMENT,
-            name text,
-            PRIMARY KEY (ingredient_id)
-          ); TRUNCATE TABLE ingredients`;
-          con.query(sql, (err, res) => {
-            if (err) throw err;
-            console.log("Table ingredients created");
-
-            console.log("Inserting ingredients...");
-
-
-            console.log("Creating table recipes...");
-            sql =
-            `CREATE TABLE IF NOT EXISTS recipes (
-              recipe_id int NOT NULL AUTO_INCREMENT,
-              title text,
-              PRIMARY KEY (recipe_id)
-            ); TRUNCATE TABLE recipes`;
-            con.query(sql, (err, res) => {
-              if (err) throw err;
-              console.log("Table recipes created");
-
-            });
-
-            console.log("Creating table users...");
-            sql =
-            `CREATE TABLE IF NOT EXISTS users (
-              user_id int NOT NULL AUTO_INCREMENT,
-              password varchar(255) NOT NULL,
-              PRIMARY KEY (user_id)
-            ); TRUNCATE TABLE users`;
-            con.query(sql, (err, res) => {
-              if (err) throw err;
-              console.log("Table users created");
-
-              console.log("Creating table user_tags...");
-              sql =
-              `CREATE TABLE IF NOT EXISTS user_tags (
-                id int NOT NULL AUTO_INCREMENT,
-                tag text,
-                PRIMARY KEY (id)
-              ); TRUNCATE TABLE user_tags`;
-              con.query(sql, (err, res) => {
-                if (err) throw err;
-                console.log("Table user_tags created");
-              });
-
-              console.log("Creating table user_ingredients...");
-              sql =
-              `CREATE TABLE IF NOT EXISTS user_ingredients (
-                id int NOT NULL AUTO_INCREMENT,
-                ingredient_id int NOT NULL,
-                amount int,
-                PRIMARY KEY (id),
-                FOREIGN KEY (ingredient_id)
-              ); TRUNCATE TABLE user_ingredients`;
-              con.query(sql, (err, res) => {
-                if (err) throw err;
-                console.log("Table user_ingredients created");
-              });
-            });
-          });
-        });
-      });
-    }
-    catch (error) {
-      console.error(error.message);
-    }
-  });
-}).on("error", (error) => {
-  console.error(error.message);
-});
-*/
