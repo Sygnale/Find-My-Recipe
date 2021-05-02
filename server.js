ac983642ecae003d00996f0f721f9c46f2076ae5
chirag-singh1 <66097224+chirag-singh1@users.noreply.github.com>
2021-05-02 15:04:53 -0700

    Documentation, added the API to add/authenticate users

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql2');
 
+//Open connection to MySQL database
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -11,6 +12,7 @@ con.connect(function (err) {
     console.log("Connected to MySQL Database");
 });
 
+//Setup express server
 const express = require('express');
 const cors = require('cors');
 
@@ -23,6 +25,69 @@ app.get('/', (req, res) => {
     });
 });
 
+/** ---------------------------------------- Backend APIs --------------------------------------- */
+
+//Method to add user to database
+//Usage (from frontend): http://localhost:8080/add-user/[username]-[password]
+//@returns user ID (in format  {"id":[id]}) for success (for use in later frontend requests), relevant error message for failure (see lines with res.end)
+app.get('/add-user/:userId-:userPassword', (req, res) => {
+    console.log("Request to add user: "+req.params.userId+" with password "+req.params.userPassword);
+
+    //Basic data validation, probably not necessary as this should be done in the frontend
+    if(req.params.userId==null || req.params.userId == "" ){ //Note that this may not actually be necessary, any bad requests probably would not even be routed here
+        res.end("Invalid username");
+        return;
+    }
+    if(req.params.userPassword ==  null || req.params.userPassword == ""){
+        res.end("Invalid password");
+        return;
+    }
+
+    var countQueryString = "SELECT COUNT (*) FROM users WHERE userid=\""+req.params.userId+"\"";
+    con.query(countQueryString, function(err, result){ //Check if user already exists
+        if(err) throw err;
+        if(result[0]["COUNT (*)"] != 0){
+            console.log("Request denied: user already found in database"); 
+            res.end("Username already exists"); //User exists, return error
+        }
+        else{ //User does not exist, insert into database with null ingredients/tags and with corresponding userID and password
+            var addQueryString = "INSERT INTO users (userid, hashed_password) VALUES (\""+req.params.userId+"\",\""+req.params.userPassword+"\")";
+            con.query(addQueryString, function(err, result){
+                if(err) throw err;
+                console.log(result);
+                var idStruct = {
+                    id: result.insertId,
+                };
+                res.end(JSON.stringify(idStruct));
+            });
+        }
+    });
+});
+
+//Method to authenticate user from database
+//Usage (from frontend): http://localhost:8080/authenticate-user/[username]-[password]
+//@returns "Authentication failed" for failure, user ID in struct in format {"id":[id]} for authenticated user for future frontend requests
+app.get('/authenticate-user/:userId-:userPassword', (req, res) => {
+    console.log("Request to authenticate user: "+req.params.userId+" with password " +req.params.userPassword);
+
+    var queryString="SELECT id FROM users WHERE userid=\""+req.params.userId+"\" AND hashed_password=\""+req.params.userPassword+"\"";
+    console.log(queryString);
+    con.query(queryString, function(err, result){
+        if(err) throw err;
+        if(result.length == 0){
+            console.log("Authentication Failed"); 
+            res.end("Authentication failed"); //Username/password does not match
+        }
+        else{
+            console.log("Authentication success");
+            var idStruct = {
+                id: result[0].id,
+            };
+            res.end(JSON.stringify(idStruct)); //Success
+        }
+    });
+});
+
 app.get('/ingredients', (req, res) => {
     console.log("Sending ingredients list")
     con.query("SELECT * FROM INGREDIENTS", function (err, result) {
@@ -31,6 +96,9 @@ app.get('/ingredients', (req, res) => {
     });
 });
 
+/** -------------------------- End of Backend APIs ----------------------------------  */ 
+
+//Start server
 app.listen(8080, () => {
     console.log('Server listening on port 8080');
 });
