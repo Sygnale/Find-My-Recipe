6628bda9e3df287dfce6ce9560e1a8ecad77b8ab
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2021-05-02 01:58:45 -0700

    inserted ingredients

--- reload_db.js
+++ reload_db.js
@@ -1,53 +1,125 @@
 const http = require('http');
 const mysql = require('mysql2');
 
-console.log("reload_db running");
+async function get_json(url) {
+  return new Promise((resolve, reject) => {
+    console.log("Getting http request...");
+    http.get(url, (res) => {
+      console.log("Http request recieved");
 
-// Make MySQL connection
-const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "123456",
-  multipleStatements: true,
-});
+      let body = "";
 
-// Get json
-let url = "http://data.csail.mit.edu/im2recipe/recipes_with_nutritional_info.json";
-url = "http://localhost:8080/";
+      console.log("Loading data...");
+      res.on("data", (chunk) => {
+        body += chunk;
+      });
 
-console.log("Getting http request...");
-http.get(url, (res) => {
-  console.log("Http request recieved");
+      res.on("end", () => {
+        console.log("Data loaded");
 
-  let body = "";
-
-  console.log("Loading data...");
-  res.on("data", (chunk) => {
-    body += chunk;
+        console.log("Parsing json...");
+        let json;
+        try {
+          json = JSON.parse(body);
+          console.log("Json parsed");
+        }
+        catch (err) {
+          console.error(err.message);
+        }
+        resolve(json);
+      });
+    }).on("error", (err) => {
+      console.error(err.message);
+    });
   });
+}
 
-  res.on("end", () => {
-    console.log("Data loaded");
+async function connect_mysql(con) {
+  return new Promise((resolve, reject) => {
+    console.log("Connecting to root@localhost...");
+    con.connect((err) => {
+      if (err) throw err;
+      console.log("Connected to root@localhost");
+      resolve();
+    });
+  });
+}
 
-    console.log("Parsing json...");
-    try {
-      let json = JSON.parse(body);
-      console.log("Json parsed");
+async function create_db(con) {
+  return new Promise((resolve, reject) => {
+    console.log("Creating database find_my_recipe...");
+    const sql = "CREATE DATABASE IF NOT EXISTS find_my_recipe; USE find_my_recipe";
+    con.query(sql, (err, res) => {
+      if (err) throw err;
+      console.log("Database find_my_recipe created");
+      resolve();
+    });
+  });
+}
 
-      // Connect to MySQL
-      console.log("Connecting to root@localhost...");
-      con.connect((err) => {
-        if (err) throw err;
-        console.log("Connected to root@localhost");
+async function insert_ingredient(con, name) {
+  return new Promise((resolve, reject) => {
+    const sql = `INSERT INTO ingredients (name) VALUES ('${name}')`;
+    con.query(sql, (err, res) => {
+      if (err && err.code !== "ER_DUP_ENTRY") throw err;
+      resolve();
+    });
+  });
+}
 
-        let sql = "";
+async function create_ingredients(con, json) {
+  return new Promise((resolve, reject) => {
+    console.log("Creating table ingredients...");
+    const sql = `CREATE TABLE IF NOT EXISTS ingredients (
+      ingredient_id int NOT NULL AUTO_INCREMENT,
+      name varchar(255),
+      PRIMARY KEY (ingredient_id),
+      UNIQUE (name)
+    ); TRUNCATE TABLE ingredients`;
+    con.query(sql, (err, res) => {
+      if (err) throw err;
+      console.log("Table ingredients created");
 
-        console.log("Creating database find_my_recipe...");
-        sql = "CREATE DATABASE IF NOT EXISTS find_my_recipe; USE find_my_recipe";
-        con.query(sql, (err, res) => {
-          if (err) throw err;
-          console.log("Database find_my_recipe created");
+      console.log("Inserting ingredients...");
+      Promise.all(json.flatMap(async (recipe) => {
+        recipe.ingredients.map(async (ingredient) => {
+          await insert_ingredient(con, ingredient.text);
+        });
+      }));
+      console.log("Ingredients inserted");
+      resolve();
+    });
+  });
+}
 
+async function reload_db() {
+  console.log("reload_db running...");
+
+  // Get json
+  let url = "http://data.csail.mit.edu/im2recipe/recipes_with_nutritional_info.json";
+  url = "http://localhost:8080/";
+  let json = await get_json(url);
+
+  // Connect to MySQL
+  const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    multipleStatements: true,
+  });
+  await connect_mysql(con);
+
+  // Reload database
+  await create_db(con);
+  await create_ingredients(con, json);
+
+  console.log("reload_db finished successfully");
+}
+
+reload_db().catch((err) => {
+  console.error(err.message);
+});
+/*
           console.log("Creating table ingredients...");
           sql =
           `CREATE TABLE IF NOT EXISTS ingredients (
@@ -60,7 +132,7 @@ http.get(url, (res) => {
             console.log("Table ingredients created");
 
             console.log("Inserting ingredients...");
-            
+
 
             console.log("Creating table recipes...");
             sql =
@@ -123,3 +195,4 @@ http.get(url, (res) => {
 }).on("error", (error) => {
   console.error(error.message);
 });
+*/
