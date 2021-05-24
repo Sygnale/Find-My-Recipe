6468e8a2724f78707d52ef343d1e163b6f9680fd
nikihosseinian <nikihosseinian@ucla.edu>
2021-05-24 12:06:47 -0700

    Create Tags.js

---
+++ find-my-recipe/src/pages/Tags.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Tags extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tags: [],
+      tagsChanged: false,
+      action: null,
+      query: null,
+      error: null,
+    };
+  }
+
+  render() {
+    return (
+      <div className='Tags'>
+        <h1>Search</h1>
+      </div>
+    );
+  }
+}
+
+export default Tags;
